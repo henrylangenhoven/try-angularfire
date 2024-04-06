b646d3909c40593a5651bd8f75c271faa6596308
Henry Langenhoven <henrylangenhoven8@gmail.com>
2024-04-06 09:30:38 +0200

    remove commited configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,18 +4,12 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {environment} from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(provideFirebaseApp(() => initializeApp({
-      "projectId": "try-angularfire",
-      "appId": "1:631127401014:web:358812f4901356acf96e97",
-      "storageBucket": "try-angularfire.appspot.com",
-      "apiKey": "AIzaSyA_Ydd3sO2jJUK5suwb2la_l3J-1xsBQEQ",
-      "authDomain": "try-angularfire.firebaseapp.com",
-      "messagingSenderId": "631127401014"
-    }))),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))),
     importProvidersFrom(provideFirestore(() => getFirestore())),
   ]
 };
