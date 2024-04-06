32ef2b918b866450ed2a48accf240d19303dea9b
Henry Langenhoven <henrylangenhoven8@gmail.com>
2024-04-06 10:27:43 +0200

    moved app config to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    projectId: 'your-project-id',
-    appId: 'your-app-id',
-    storageBucket: 'your-storage-bucket',
-    apiKey: 'your-api-key',
-    authDomain: 'your-auth-domain',
-    messagingSenderId: 'your-messaging-sender-id',
+    "projectId": "try-angularfire",
+    "appId": "1:631127401014:web:358812f4901356acf96e97",
+    "storageBucket": "try-angularfire.appspot.com",
+    "apiKey": "AIzaSyA_Ydd3sO2jJUK5suwb2la_l3J-1xsBQEQ",
+    "authDomain": "try-angularfire.firebaseapp.com",
+    "messagingSenderId": "631127401014"
   }
 };
