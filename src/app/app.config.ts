4506ebf89e2d3742481df3cc44cfb46ba5ffa8e0
Henry Langenhoven <henrylangenhoven8@gmail.com>
2024-04-06 09:03:46 +0200

    added dotenv and project config via secrets

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,12 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {environment} from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))),
+    importProvidersFrom(provideFirestore(() => getFirestore()))
+  ]
 };
