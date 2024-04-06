6a008d998d3a47ba36a5da95274d8c3a84c31f3d
Henry Langenhoven <henrylangenhoven8@gmail.com>
2024-04-06 09:18:40 +0200

    formatting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,6 +10,14 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))),
-    importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"try-angularfire","appId":"1:631127401014:web:358812f4901356acf96e97","storageBucket":"try-angularfire.appspot.com","apiKey":"AIzaSyA_Ydd3sO2jJUK5suwb2la_l3J-1xsBQEQ","authDomain":"try-angularfire.firebaseapp.com","messagingSenderId":"631127401014"}))), importProvidersFrom(provideFirestore(() => getFirestore()))
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({
+      "projectId": "try-angularfire",
+      "appId": "1:631127401014:web:358812f4901356acf96e97",
+      "storageBucket": "try-angularfire.appspot.com",
+      "apiKey": "AIzaSyA_Ydd3sO2jJUK5suwb2la_l3J-1xsBQEQ",
+      "authDomain": "try-angularfire.firebaseapp.com",
+      "messagingSenderId": "631127401014"
+    }))), importProvidersFrom(provideFirestore(() => getFirestore()))
   ]
 };
