ed5fa26c701e77f3df73ffda861bd2b87418a563
Henry Langenhoven <henrylangenhoven8@gmail.com>
2024-04-06 09:25:35 +0200

    read from firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {collection, collectionData, Firestore} from '@angular/fire/firestore';
+import {Observable} from 'rxjs';
+import {AsyncPipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'try-angularfire';
+
+  firestore: Firestore = inject(Firestore)
+  items$: Observable<any[]>;
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'items')
+    this.items$ = collectionData(aCollection);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,10 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
-import {environment} from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))),
-    importProvidersFrom(provideFirestore(() => getFirestore())),
     importProvidersFrom(provideFirebaseApp(() => initializeApp({
       "projectId": "try-angularfire",
       "appId": "1:631127401014:web:358812f4901356acf96e97",
@@ -18,6 +15,7 @@ export const appConfig: ApplicationConfig = {
       "apiKey": "AIzaSyA_Ydd3sO2jJUK5suwb2la_l3J-1xsBQEQ",
       "authDomain": "try-angularfire.firebaseapp.com",
       "messagingSenderId": "631127401014"
-    }))), importProvidersFrom(provideFirestore(() => getFirestore()))
+    }))),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
   ]
 };
